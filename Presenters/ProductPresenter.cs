3779fa06b43a0d3e2f6b716dd1d270705073c8d3
Alexandro Yule Valencia <alexandroyule@gmail.com>
2023-10-20 14:12:56 -0500

    ProductPresenter

---
+++ Presenters/ProductPresenter.cs
@@ -0,0 +1,142 @@
+﻿using Supermarket_mvp.Models;
+using Supermarket_mvp.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Presenters
+{
+    internal class ProductPresenter
+    {
+        private IProductView view;
+        private IProductRepository repository;
+        private BindingSource productBindingSource;
+        private IEnumerable<ProductModel> productList;
+
+        public ProductPresenter(IProductView view, IProductRepository repository)
+        {
+            this.productBindingSource = new BindingSource();
+
+            this.view = view;
+            this.repository = repository;
+
+            this.view.SearchEvent += SearchProduct;
+            this.view.AddNewEvent += AddNewProduct;
+            this.view.EditEvent += LoadSelectProductToEdit;
+            this.view.DeleteEvent += DeleteSelectedProduct;
+            this.view.SaveEvent += SaveProduct;
+            this.view.CancelEvent += CancelAction;
+
+            this.view.SetProductListBildingSource(productBindingSource);
+
+            loadAllProductList();
+
+            this.view.Show();
+        }
+
+        private void loadAllProductList()
+        {
+            productList = repository.GetAll();
+            productBindingSource.DataSource = productList;
+        }
+
+        private void CancelAction(object? sender, EventArgs e)
+        {
+            CleanViewFields();
+        }
+
+        private void CleanViewFields()
+        {
+            view.ProductId = "0";
+            view.ProductName = "";
+            view.ProductPrice = "";
+            view.ProductStock = "";
+        }
+
+        private void SaveProduct(object? sender, EventArgs e)
+        {
+            var Product = new ProductModel();
+            Product.Id = Convert.ToInt32(view.ProductId);
+            Product.Name = view.ProductName;
+            Product.Price = Convert.ToInt32(view.ProductPrice);
+            Product.Stock = Convert.ToInt32(view.ProductStock);
+
+            try
+            {
+                new Common.ModelDataValidation().Validate(Product);
+                if (view.IsEdit)
+                {
+                    repository.Edit(Product);
+                    view.Message = "Product edited successfully";
+                }
+                else
+                {
+                    repository.Add(Product);
+                    view.Message = "Product added successfully";
+                }
+                view.IsSuccessful = true;
+                loadAllProductList();
+                CleanViewFields();
+            }
+            catch (Exception ex)
+            {
+                view.IsSuccessful = false;
+                view.Message = ex.Message;
+
+            }
+        }
+
+        private void DeleteSelectedProduct(object? sender, EventArgs e)
+        {
+            try
+            {
+                var product = (ProductModel)productBindingSource.Current;
+
+                repository.Delete(product.Id);
+                view.IsSuccessful = true;
+                view.Message = "Pay Mode deleted successfully";
+                loadAllProductList();
+            }
+            catch (Exception ex)
+            {
+                view.IsSuccessful = false;
+                view.Message = "An error ocurred, could not delete pay mode";
+            }
+        }
+
+        private void LoadSelectProductToEdit(object? sender, EventArgs e)
+        {
+            var product = (ProductModel)productBindingSource.Current;
+
+            view.ProductId = product.Id.ToString();
+            view.ProductName = product.Name;
+            view.ProductPrice = product.Price.ToString();
+            view.ProductStock = product.Stock.ToString();
+
+            view.IsEdit = true;
+        }
+
+        private void AddNewProduct(object? sender, EventArgs e)
+        {
+            view.IsEdit = false;
+            CleanViewFields();
+
+        }
+
+        private void SearchProduct(object? sender, EventArgs e)
+        {
+            bool emptyValue = string.IsNullOrWhiteSpace(this.view.SearchValue);
+            if (emptyValue == false)
+            {
+                productList = repository.GetByValue(this.view.SearchValue);
+            }
+            else
+            {
+                productList = repository.GetAll();
+            }
+            productBindingSource.DataSource = productList;
+        }
+    }
+}
